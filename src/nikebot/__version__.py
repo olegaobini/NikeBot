39ffe58d04cdabc2e1212ff0b2611817c30a37e8
olegaobini <obiniolega@gmail.com>
2022-02-10 16:13:47 -0800

    boilerplate from Insomniac repo

---
+++ src/nikebot/__version__.py
@@ -0,0 +1,21 @@
+__logo__ = """
+##########################################################
+#                                                        #
+#   _____                                 _              #
+#  |_   _|                               (_)             #
+#    | |  _ __  ___  ___  _ __ ___  _ __  _  __ _  ___   #
+#    | | | '_ \/ __|/ _ \| '_ ` _ \| '_ \| |/ _` |/ __|  #
+#   _| |_| | | \__ \ (_) | | | | | | | | | | (_| | (__   #
+#  |_____|_| |_|___/\___/|_| |_| |_|_| |_|_|\__,_|\___|  #
+#                                                        #
+##########################################################
+"""
+__title__ = 'nikebot'
+__description__ = 'Simple Instagram bot for automated Instagram interaction using Android.'
+__url__ = 'https: // github.com/olegaobini/NikeBot'
+__version__ = '3.7.28'
+__debug_mode__ = False
+__author__ = 'Olega Obini'
+__author_email__ = 'obiniolega@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 Olega Obini'
